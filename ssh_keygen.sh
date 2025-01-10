78f17c1dc98ce3981c27f8d39641b250dda2befd
Fardin Islam <31590240+24phyr@users.noreply.github.com>
2025-01-10 18:38:58 +0600

    Create ssh_keygen.sh

---
+++ ssh_keygen.sh
@@ -0,0 +1,13 @@
+!#/bin/bash
+
+generate_ssh_key() {
+  echo "Generating SSH key for $(whoami)"
+
+  if ssh-keygen -t rsa -f ~/.ssh/$(whoami)_id_rsa -N ""; then
+    echo "SSH key generated successfully for $(whoami)"
+  else
+    echo "SSH key generation failed"
+  fi
+}
+
+generate_ssh_key
